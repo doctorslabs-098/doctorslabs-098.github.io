de6979230c1fec18785bbe10955da21d85954f3b
Ivan <vanikapo01@gmail.com>
2025-09-02 07:16:35 +0400

    Cайт-0032

---
+++ script.js
@@ -0,0 +1,7 @@
+// Пример: Вывод сообщения в консоль при загрузке страницы
+console.log("Сайт загружен!");
+
+// Пример: Alert при клике на заголовок
+document.querySelector('h1').addEventListener('click', function() {
+    alert('Вы кликнули на заголовок!');
+});
